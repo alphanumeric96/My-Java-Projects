c128ce5e62d3be1e5de6986840234112dd479edd
alphanumeric96 <shoryak2005@outlook.com>
2021-03-27 15:49:21 +0530

    Updates

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+/**
+ * @author Shorya created on 27 March 2021
+ * 
+ */
+
+public class Modulus {
+    public static void main(String[] args) {
+        int x = 95;
+        int y = 6;
+        System.out.println(x % y);// Output : 5
+    }
+}
---
+++ XPlusY.java
@@ -0,0 +1,12 @@
+/**
+ * @author Shorya created on 27 March 2021
+ * X + Y
+ */
+
+public class XPlusY {
+    public static void main(String[] args) {
+        int x = 95;
+        int y = 5;
+        System.out.println(x + y);// Output : 100
+    }
+}
