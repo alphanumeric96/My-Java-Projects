0da698c944f19c61744716d5ce7c6d015677c6b7
alphanumeric96 <shoryak2005@outlook.com>
2021-03-27 16:55:32 +0530

    Switched to IntelliJ Platform and Learning Strings

---
+++ Strings.java
@@ -0,0 +1,32 @@
+import java.sql.Time;
+
+/**
+ * @author Shorya created on 27 March 2021
+ * Learning Strings
+ */
+
+public class Strings {
+    public static void main(String[] args) {
+        String p = "QWERTY is a Keyboard style";
+        System.out.println("The total length of the string is:" + p.length());
+        System.out.println("To Upper Case:" + p.toUpperCase());
+        System.out.println("To Lower case:" + p.toLowerCase());
+        System.out.println(p.indexOf("Key"));
+        String q = "SomeName";
+        String r = "DifferentName";
+        System.out.println(q + " " + r);
+
+        String txt = "Hello\rWorld!";
+        System.out.println(txt);
+
+        String tabString = "Hello\tWorld!";
+        System.out.println(tabString);
+
+        int x = 10;
+        int y = 20 + x;
+        System.out.println(y);
+
+        System.out.println(Math.floor(Math.random()*1000000L + 1));
+
+    }
+}
---
+++ out/production/My-Java-Projects/README.md
@@ -0,0 +1,2 @@
+# My Java Projects
+ My Java Programs
