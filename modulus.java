cec9cf99c611a325f2ed814e5dca1510ab8042e0
alphanumeric96 <shoryak2005@outlook.com>
2021-03-27 15:13:38 +0530

    use of plus minus into and modulus
    Description is in the title

---
+++ modulus.java
@@ -0,0 +1,12 @@
+/**
+ * @author Shorya created on 26 March 2021
+ * 
+ */
+
+public class modulus {
+    public static void main(String[] args) {
+        int x = 95;
+        int y = 6;
+        System.out.println(x % y);
+    }
+}
---
+++ xandy.java
@@ -0,0 +1,12 @@
+/**
+ * @author Shorya created on 26 March 2021
+ * 
+ */
+
+public class xandy {
+    public static void main(String[] args) {
+        int x = 95;
+        int y = 5;
+        System.out.println(x + y);
+    }
+}
