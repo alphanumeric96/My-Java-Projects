36cc778c4f21bccb2e145547af8acbf1df7eb22e
alphanumeric96 <shoryak2005@outlook.com>
2021-04-01 18:56:48 +0530

    Made a DoWhile Code

---
+++ DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int Dogs = 4;
+        do {
+            System.out.println(Dogs);//Prints the value of Integer Dogs until The value of Dogs is less than 98
+            Dogs++;
+        }
+        while (Dogs < 98);
+    }
+}
